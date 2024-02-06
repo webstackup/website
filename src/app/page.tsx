a4fe1839ad53060e8005b97262f1a7b59a351332
WebStackup <wstackup@gmail.com>
2024-02-06 21:08:38 +0700

    Update content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello WebStackup!</h1>
+    <main className="flex min-h-screen flex-col items-center justify-start px-10 py-20">
+      <h1 className="text-2xl p-2">WebStackup.com</h1>
+      <p className="text-sm p-2 text-slate-500">
+        We focuses on full stack JavaScript development using Bootstrap,
+        TypeScript, React, Next and Node technologies
+      </p>
     </main>
   );
 }
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,6 +2,8 @@
 
 import { NextUIProvider } from "@nextui-org/react";
 
-export function Providers({ children }: { children: React.ReactNode }) {
+export function Providers({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   return <NextUIProvider>{children}</NextUIProvider>;
 }
