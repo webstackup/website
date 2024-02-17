1575a103c78070b0ecb403589a5f85de1a0f3478
WebStackup <wstackup@gmail.com>
2024-02-17 15:47:28 +0700

    Add logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,25 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ReactNode } from "react";
 import "./globals.css";
 import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "WebStackup.com | Full stack JavaScript development",
+  title: "WebStackup | Full stack JavaScript development",
   description:
-    "We focuses on full stack JavaScript development using Bootstrap, TypeScript, React, Next and Node technologies",
+    "We focuses on full stack JavaScript development using TypeScript, React, Next and Node technologies",
+  keywords: "JavaScript, TypeScript, React, Next, Node, Full stack",
 };
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start px-10 py-20">
-      <h1 className="text-2xl p-2">WebStackup.com</h1>
-      <p className="text-sm p-2 text-slate-500">
-        We focuses on full stack JavaScript development using Bootstrap,
-        TypeScript, React, Next and Node technologies
-      </p>
-    </main>
+    <div className="flex min-h-screen flex-col items-center justify-start">
+      <header className="flex flex-col items-center justify-start p-5">
+        <Image src="logo.svg" alt="WebStackup" width={100} height={100} />
+        <h1 className="text-4xl bg-gradient-to-b from-[#f90] to-[#f50] inline-block text-transparent bg-clip-text">
+          WebStackup
+        </h1>
+      </header>
+      <main className="p-5">
+        <p className="text-lg p-2">
+          We focuses on full stack JavaScript development using TypeScript,
+          React, Next and Node technologies
+        </p>
+      </main>
+    </div>
   );
 }
